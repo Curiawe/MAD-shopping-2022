e6ab74b6aadfe41e552e34fd417a2c3aa49a2e16
jodh <jodh@itu.dk>
2022-02-02 13:22:07 +0100

    is that everyone, now?

---
+++ app/src/main/java/dk/itu/shopping/ShoppingActivity.java
@@ -0,0 +1,41 @@
+package dk.itu.shopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ShoppingActivity extends AppCompatActivity {
+
+    //Shopping V1
+
+    // GUI variables
+    private Button listItems;
+    private TextView items;
+
+    // Model: Database of items
+    private ItemsDB itemsDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopping);
+
+        itemsDB= new ItemsDB();
+        itemsDB.fillItemsDB();
+
+        items= findViewById(R.id.items);
+
+        listItems= findViewById(R.id.list_button);
+        listItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                items.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                items.setText("Shopping List:"+itemsDB.listItems());
+            }
+        });
+    }
+}
